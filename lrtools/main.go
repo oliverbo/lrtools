90a7f0591016d4cd8906a535af3ead58c18fdf65
oliverbo <oliver@enobo.com>
2018-02-17 14:35:54 -0500

    - Support for command line arguments - setting the database file via -lrcat parameters

--- lrtools/main.go
+++ lrtools/main.go
@@ -3,18 +3,40 @@ package main
 import (
 	"github.com/oliverbo/lrtools/lrdb"
 	"fmt"
+	"flag"
 )
+
+var cliParameters struct {
+	dbPath string
+	list bool
+}
 
 func main() {
 	// Reading and writing current configuration
 	lrdb.ReadConfig()
 	defer lrdb.WriteConfig()
 
-	lrdb.Config.DbPath = "/Volumes/Claire/Photo/Photos 2015/Photos 2015-2.lrcat"
+	parseCli()
 
+	// Set catalog name
+	if cliParameters.dbPath != "" {
+		lrdb.Config.DbPath = cliParameters.dbPath
+	}
 
 	lrdb.LoadData()
 
+	if cliParameters.list {
+		listCollection()
+	}
+}
+
+func parseCli() {
+	flag.StringVar(&cliParameters.dbPath, "lrcat", "", "Path to Lightroom catalog")
+	flag.BoolVar(&cliParameters.list, "list", false, "List collections in catalog")
+	flag.Parse()
+}
+
+func listCollection() {
 	root := lrdb.GetCollectionRoot()
 
 	root.VisitChildren(func(c *lrdb.Collection) {
